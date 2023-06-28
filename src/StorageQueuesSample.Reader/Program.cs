d9171d8b625493558558d3646c9efe725a997965
Vincent Nyanga <vince.nyanga@hotmail.com>
2023-06-28 06:48:48 +0200

    update

---
+++ src/StorageQueuesSample.Reader/Program.cs
@@ -0,0 +1,29 @@
+﻿using Coravel;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using StorageQueuesSample.Reader.Invocables;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddScheduler();
+        services.AddTransient<SendWelcomeEmail>();
+    });
+
+var host = builder.Build();
+    
+host.Services.UseScheduler(scheduler =>
+{
+    scheduler
+        .Schedule<SendWelcomeEmail>()
+        .EverySecond();
+});
+
+host.Start();
+
+Console.ReadLine();
---
+++ src/StorageQueuesSample.Writer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Coravel;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using StorageQueuesSample.Writer.Invocables;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddScheduler();
+        services.AddTransient<RegisterUser>();
+    });
+
+var host = builder.Build();
+    
+host.Services.UseScheduler(scheduler =>
+{
+    scheduler
+        .Schedule<RegisterUser>()
+        .EveryFiveSeconds();
+});
+
+host.Start();
+
+Console.ReadLine();
